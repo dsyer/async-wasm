9db105e1965a99c78fb7ecbecd352b483d0d02b0
Dave Syer <dsyer@vmware.com>
2022-06-17 14:33:59 +0100

    No need for global value

--- callback.c
+++ callback.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-int value = 0;
-
 void get(int input, int (*callback)(int));
 
 int callback(int input, int (*callback)(int)) {
@@ -15,7 +13,7 @@ int callback2(int input) {
 	return input + 2;
 }
 
-int call(int input)
+void call(int input)
 {
 	int (*callback)(int);
 	if (input<2) {
@@ -24,5 +22,4 @@ int call(int input)
 		callback = callback2;
 	}
 	get(input, callback);
-	return value;
 }
